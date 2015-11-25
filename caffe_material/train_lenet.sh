b1ab0db28dd3063344c145740290d234160e8708
Daniel Seita <takeshidanny@gmail.com>
2015-11-24 20:55:05 -0800

    Reorganizing directory a bit, added normal size for the sake of comparision and to check correctness

--- caffe_material/train_lenet.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train --solver=ee227bt_project/caffe_material/downscaled_solver.prototxt
---
+++ caffe_material/train_normalsize_lenet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+./build/tools/caffe train --solver=ee227bt_project/caffe_material/lenet_solver.prototxt
